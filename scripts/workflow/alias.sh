6834764b94b889f8570dd99715bee39fd439cd1e
V N <vee.vidify@gmail.com>
2024-10-06 18:04:12 +1100

    upd

--- scripts/workflow/alias.sh
+++ scripts/workflow/alias.sh
@@ -2,5 +2,6 @@ alias k="kubectl"
 alias copy="xclip -selection clipboard -in"
 alias use="mise use -g"
 alias kit="kitty --detach"
+alias icat="kitten icat"
 #alias dotfiles='/usr/bin/git --git-dir=$HOME/devs/dots --work-tree=$HOME'
 
